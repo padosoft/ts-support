601f9f76f38c673ed2d96be96ddac0ef917be698
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-09-10 19:27:12 +0200

    chore: fix defaultCustomExports type

--- packages/config/compiler/tsdown.ts
+++ packages/config/compiler/tsdown.ts
@@ -5,18 +5,19 @@ import {
 	type UserConfigFn,
 } from "tsdown";
 
-export const defaultCustomExports: NonNullable<ExportsOptions>["customExports"] =
-	(exports) => {
-		const fixed: typeof exports = {};
+export const defaultCustomExports: NonNullable<
+	ExportsOptions["customExports"]
+> = (exports) => {
+	const fixed: typeof exports = {};
 
-		for (const [key, value] of Object.entries(exports)) {
-			const normalizedKey = key.replace(/\\/g, "/");
-			const normalizedValue = value.replace(/\\/g, "/");
-			fixed[normalizedKey] = normalizedValue;
-		}
+	for (const [key, value] of Object.entries(exports)) {
+		const normalizedKey = key.replace(/\\/g, "/");
+		const normalizedValue = value.replace(/\\/g, "/");
+		fixed[normalizedKey] = normalizedValue;
+	}
 
-		return fixed;
-	};
+	return fixed;
+};
 
 export const tsdown = (packageOptions?: Options): UserConfigFn => {
 	return defineConfig((overrideOptions) => {
