7694cf7d407907d75b6e3abf653d4047e1356908
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-09-10 19:00:26 +0200

    chore: add defaultCustomExports tsdown fn

--- packages/config/compiler/tsdown.ts
+++ packages/config/compiler/tsdown.ts
@@ -1,4 +1,22 @@
-import { defineConfig, type Options, type UserConfigFn } from "tsdown";
+import {
+	defineConfig,
+	type ExportsOptions,
+	type Options,
+	type UserConfigFn,
+} from "tsdown";
+
+export const defaultCustomExports: NonNullable<ExportsOptions>["customExports"] =
+	(exports) => {
+		const fixed: typeof exports = {};
+
+		for (const [key, value] of Object.entries(exports)) {
+			const normalizedKey = key.replace(/\\/g, "/");
+			const normalizedValue = value.replace(/\\/g, "/");
+			fixed[normalizedKey] = normalizedValue;
+		}
+
+		return fixed;
+	};
 
 export const tsdown = (packageOptions?: Options): UserConfigFn => {
 	return defineConfig((overrideOptions) => {
@@ -9,6 +27,15 @@ export const tsdown = (packageOptions?: Options): UserConfigFn => {
 
 		if (Array.isArray(options.entry)) {
 			options.entry.push("!dist");
+		}
+
+		/**
+		 * Apply default exports transformation
+		 */
+		if (options.exports && typeof options.exports === "boolean") {
+			options.exports = {
+				customExports: defaultCustomExports,
+			};
 		}
 
 		return {
