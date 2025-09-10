89c43439d5443c9bcf12a10679b2eab80eb1a986
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-09-10 19:37:53 +0200

    chore(config): add dynamic package.json exports

--- packages/config/tsdown.config.ts
+++ packages/config/tsdown.config.ts
@@ -1,10 +1,45 @@
+import { readdir } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import type { UserConfigFn } from "tsdown";
-import { tsdown } from "./compiler/tsdown.ts";
+import { defaultCustomExports, tsdown } from "./compiler/tsdown.ts";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const assetsDirs = ["typescript", "tools"];
+const assetsExts = ["json"];
 
 const config: UserConfigFn = tsdown({
 	entry: ["*/**/*.ts"],
 	unbundle: true,
 	external: ["tsdown", "rolldown"],
+	exports: {
+		customExports: async (exports, context) => {
+			const assetExports: Record<string, string> = {
+				...exports,
+			};
+
+			for (const dir of assetsDirs) {
+				const absDir = path.join(__dirname, dir);
+				const files = await readdir(absDir);
+
+				for (const file of files) {
+					const ext = path.extname(file).slice(1);
+					console.log(path.extname(file), ext);
+
+					if (!assetsExts.includes(ext)) continue;
+
+					const base = path.basename(file, `.${ext}`);
+					const relPath = `./${dir}/${base}`;
+					const fullRelPath = `./${dir}/${file}`;
+
+					assetExports[relPath] = fullRelPath;
+				}
+			}
+
+			return await defaultCustomExports(assetExports, context);
+		},
+	},
 });
 
 export default config;
