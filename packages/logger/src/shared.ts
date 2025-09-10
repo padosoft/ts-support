b20e5ccd3f3980535fa4947166bc7574e91b9267
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-09-11 00:02:24 +0200

    chore: clean shared logger

--- packages/logger/src/shared.ts
+++ packages/logger/src/shared.ts
@@ -1,21 +1,3 @@
-import { Logger } from "./core/logger";
-import { rateLimiterPlugin } from "./plugins/rate-limiter";
-import { consoleTransport } from "./transports/console";
+import { Logger } from "@/core/logger";
 
-export const logger: Logger = new Logger({
-	transports: [consoleTransport()],
-});
-
-logger.setBatching({
-	maxBatchSize: 10,
-});
-
-//logger.addPlugin(rateLimiterPlugin(1000));
-logger.info("booting...");
-console.log("hey");
-logger.debug("debug info");
-logger.error("oops");
-console.log("hey2");
-logger.error("oops2");
-
-logger.flush();
+export const logger: Logger = new Logger();
